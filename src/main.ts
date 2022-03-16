4cfb19c3fa5ac51be7f888e9e23ad887fd69b2b3
Yura Lapitski <lyurik@tut.by>
2022-03-16 22:09:45 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Blog Server example')
+    .setDescription('The Blog Server API description')
+    .setVersion('1.0')
+    .addTag('Blog Server')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
